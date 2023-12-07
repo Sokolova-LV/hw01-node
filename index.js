56c798e9787145ae550ecb0e9d188eeed0d78c34
Sokolova-LV <leyla-@ukr.net>
2023-12-07 09:06:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const contacts = require("./contacts");
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case 'list':
+            const allContacts = await contacts.listContacts();
+            return console.log(allContacts);
+        case 'get':
+            const oneContact = await contacts.getContactById();
+            return console.log(oneContact);
+        case 'add':
+            const newContact = await contacts.addContact({ name, email, phone });
+            return console.log(newContact);
+        case 'remove':
+            const deleteContact = await contacts.removeContact(id);
+            return console.log(deleteContact);
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
+
+/*# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+node index.js --action="list"
+
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH*/
